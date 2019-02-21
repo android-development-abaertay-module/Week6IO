930e762cbc664322bbe3e02562a34c957d2fae0f
Bodovix <stbbap@gmail.com>
2019-02-21 09:16:44 +0000

    added project files

---
+++ app/src/main/java/com/bodovix/week6localstorage/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bodovix.week6localstorage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
